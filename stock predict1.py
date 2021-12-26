e502fa391a9d53302e099e7178523e8e0c8c1193
khaledyasser2 <41228415+khaledyasser2@users.noreply.github.com>
2021-12-26 23:04:18 +0900

    Update stock predict1.py

--- stock predict1.py
+++ stock predict1.py
@@ -67,7 +67,7 @@ model.fit(x_train, y_train, epochs=25, batch_size=32)
 # model.fit(x_train, y_train, epochs=25, batch_size=32)
 
 # Load Test Data
-test_start = dt.datetime(2020 1,1)
+test_start = dt.datetime(2020,1,1)
 test_end = dt.datetime.now()
 
 test_data = web.DataReader(company, 'yahoo', test_start, test_end)
@@ -76,8 +76,7 @@ actual_prices = test_data('Close').values
 total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
 
 total_dataset = pd.concat((data['Close'], test_data[ 'Close']), axis=0)
-model_inputs = total_dataset[len(total_dataset) - len(test_data)]
-prediction_days:1.value
+model_inputs = total_dataset[len(total_dataset) - len(test_data)-prediction_days:].value
 model_inputs = model_inputs.reshape(-1, 1)
 model_inputs = scaler.transform (model_inputs)
 
