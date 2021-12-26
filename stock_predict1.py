29f60a6fbf3924269f7a996ad2b3aa4da5d4c03a
khaledyasser2 <41228415+khaledyasser2@users.noreply.github.com>
2021-12-27 00:11:43 +0900

    it works now

--- stock_predict1.py
+++ stock_predict1.py
@@ -3,7 +3,9 @@
 
 # In[3]:
 
-
+#You need python 3.9 for tensorflow to work.
+#if you get any errors here, just type pip install *the thing that's giving an error* in a console
+#for tensorflow you might need to put: pip install --upgrade https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.12.0-py3-none-any.whl
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -16,8 +18,8 @@ from tensorflow.keras.layers import Dense,Dropout, LSTM
 #Load Data
 company = "FB"
 
-start = dt.datetime(2021,1,1)
-end = dt.datetime(2020,1,1)
+start = dt.datetime(2020,1,1)
+end = dt.datetime(2021,1,1)
 
 data = web.DataReader(company, 'yahoo', start, end)
 scaler = MinMaxScaler(feature_range=(0,1))
@@ -37,7 +39,7 @@ x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1],1))
 
 model = Sequential()
 
-model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train,shape[1],1)))
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1],1)))
 model.add(Dropout(0.2))
 model.add(LSTM(units=50, return_sequences=True))
 model.add(Dropout(0.2))
@@ -67,34 +69,34 @@ model.fit(x_train, y_train, epochs=25, batch_size=32)
 # model.fit(x_train, y_train, epochs=25, batch_size=32)
 
 # Load Test Data
-test_start = dt.datetime(2020,1,1)
+test_start = dt.datetime(2021,1,1)
 test_end = dt.datetime.now()
 
 test_data = web.DataReader(company, 'yahoo', test_start, test_end)
-actual_prices = test_data('Close').values
+actual_prices = test_data['Close'].values
 
 total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
 
 total_dataset = pd.concat((data['Close'], test_data[ 'Close']), axis=0)
-model_inputs = total_dataset[len(total_dataset) - len(test_data)-prediction_days:].value
+model_inputs = total_dataset[len(total_dataset) - len(test_data)-prediction_days:].values
 model_inputs = model_inputs.reshape(-1, 1)
 model_inputs = scaler.transform (model_inputs)
 
 # Make Predictions on Test Data
 x_test = []
 
-for x in range (prediction_days, Len (model_inputs)):
+for x in range (prediction_days, len(model_inputs)):
     x_test.append (model_inputs [x-prediction_days:x, 0])
 
-X_test = np.array (x_test)
-x_test = np.reshape (x_test, (x_test.shape[0], x_test.shape [1], 1))
+x_test = np.array (x_test)
+x_test = np.reshape (x_test, (x_test.shape[0], x_test.shape[1], 1))
 predicted_prices = model.predict(x_test)
 predicted_prices = scaler.inverse_transform (predicted_prices)
 
 # Plot The Test Predictions
-plt.plot(actual_prices, color="black", Label=f"Actual {company} Price")
+plt.plot(actual_prices, color="black", label=f"Actual {company} Price")
 plt.plot(predicted_prices, color='green', label=f"Predicted {company} Price")
-plt.title(f'{company} Share Price')
+plt.title(f"{company} Share Price")
 plt.xlabel('Time')
 plt.ylabel(f'{company} Share Price')
 plt.legend ()
